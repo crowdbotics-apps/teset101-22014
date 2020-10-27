39236d1bf28b19ab7a5a7c65ecfba6725c296aa7
Crowdbotics <team@crowdbotics.com>
2020-10-27 18:44:39 +0000

    remove package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -5,7 +5,6 @@ export const installed_blueprints = [
 { name: 'SignIn13163484', human_name: 'Sign In 1 3', access_route: 'SignIn13163484'},
 { name: 'Settings5163482', human_name: 'Settings 5', access_route: 'Settings5163482'},
 { name: 'Messaging6163481', human_name: 'Messaging 6', access_route: 'Messaging6163481', icon: 'comments'},
-{ name: 'NotificationList8163479', human_name: 'Notification List 8', access_route: 'NotificationList8163479'},
 { name: 'Maps10163478', human_name: 'Maps 10', access_route: 'Maps10163478', icon: 'map'},
 { name: 'Camera12163476', human_name: 'Camera 12', access_route: 'Camera12163476', icon: 'camera'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -10,7 +10,6 @@ import SignUp12163485Navigator from '../features/SignUp12163485/navigator';
 import SignIn13163484Navigator from '../features/SignIn13163484/navigator';
 import Settings5163482Navigator from '../features/Settings5163482/navigator';
 import Messaging6163481Navigator from '../features/Messaging6163481/navigator';
-import NotificationList8163479Navigator from '../features/NotificationList8163479/navigator';
 import Maps10163478Navigator from '../features/Maps10163478/navigator';
 import Camera12163476Navigator from '../features/Camera12163476/navigator';
 
@@ -26,7 +25,6 @@ SignUp12163485: { screen: SignUp12163485Navigator },
 SignIn13163484: { screen: SignIn13163484Navigator },
 Settings5163482: { screen: Settings5163482Navigator },
 Messaging6163481: { screen: Messaging6163481Navigator },
-NotificationList8163479: { screen: NotificationList8163479Navigator },
 Maps10163478: { screen: Maps10163478Navigator },
 Camera12163476: { screen: Camera12163476Navigator },
 
